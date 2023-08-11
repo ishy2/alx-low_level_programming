f63c3a97d2a306e1568857beb5169025181a7aef
ishy2 <ishmaeltsorblewu@gmail.com>
2023-08-11 11:40:02 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	d = open(filename, O_WRONLY | O_APPEND);
-	b = write(o, text_content, len);
+	b = write(d, text_content, len);
 
 	if (d == -1 || w == -1)
 		return (-1);
